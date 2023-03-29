9756caa7f23547b0da7b6b8a0ac5fd27c4abf8da
Henning Ulfarsson <henningu@ru.is>
2023-03-29 13:27:57 +0000

    Setting up a theorem for the distributions

--- bisc_adjacent.py
+++ bisc_adjacent.py
@@ -55,6 +55,14 @@ def inverse_Foata(perm):
 # print(inverse_Foata(Foata(p)) == p)
 
 
+def cycle_is_increasing(c):
+    fc = fix_cycle(c)
+    for j in range(len(fc) - 1):
+        if fc[j + 1] - fc[j] != 1:
+            return False
+    return True
+
+
 def has_increasing_cycle(perm, i):
     cd = perm.cycle_decomp()
     for c in cd:
@@ -64,8 +72,18 @@ def has_increasing_cycle(perm, i):
     return False
 
 
-def cycle_is_increasing(c):
-    fc = fix_cycle(c)
+def num_increasing_cycles(perm, i):
+    cd = perm.cycle_decomp()
+    count = 0
+    for c in cd:
+        if len(c) == i:
+            if cycle_is_increasing(c):
+                count += 1
+    return count
+
+
+def cycle_is_sorted_increasing(c):
+    fc = sorted(fix_cycle(c))
     for j in range(len(fc) - 1):
         if fc[j + 1] - fc[j] != 1:
             return False
@@ -81,14 +99,99 @@ def has_increasing_sorted_cycle(perm, i):
     return False
 
 
-def cycle_is_sorted_increasing(c):
-    fc = sorted(fix_cycle(c))
-    for j in range(len(fc) - 1):
-        if fc[j + 1] - fc[j] != 1:
-            return False
-    return True
-
-
-q = 3
+q = 4
 # auto_bisc(lambda perm: not has_increasing_cycle(inverse_Foata(perm), q))
-auto_bisc(lambda perm: not has_increasing_sorted_cycle(inverse_Foata(perm), q))
+# auto_bisc(lambda perm: not has_increasing_sorted_cycle(inverse_Foata(perm), q))
+
+D = {
+    4: {
+        Perm((0, 1, 2, 3)): [
+            {
+                (0, 1),
+                (2, 4),
+                (1, 2),
+                (3, 4),
+                (2, 1),
+                (4, 3),
+                (3, 1),
+                (0, 2),
+                (2, 2),
+                (1, 0),
+                (3, 2),
+                (1, 3),
+                (4, 1),
+                (4, 4),
+                (0, 0),
+                (1, 1),
+                (0, 3),
+                (2, 0),
+                (4, 2),
+                (1, 4),
+                (2, 3),
+                (3, 0),
+                (3, 3),
+                (4, 0),  # added
+            }
+        ]
+    },
+    5: {
+        Perm((1, 2, 3, 4, 0)): [
+            {
+                (0, 1),
+                (2, 4),
+                (1, 2),
+                (0, 4),
+                (3, 4),
+                (2, 1),
+                (4, 3),
+                (1, 5),
+                (3, 1),
+                (5, 4),
+                (0, 2),
+                (2, 2),
+                (1, 0),
+                (3, 2),
+                (2, 5),
+                (1, 3),
+                (3, 5),
+                (5, 2),
+                (4, 4),
+                (0, 0),
+                (1, 1),
+                (0, 3),
+                (2, 0),
+                (4, 2),
+                (1, 4),
+                (2, 3),
+                (3, 0),
+                (4, 5),
+                (3, 3),
+                (5, 3),
+                (4, 1),  # added
+                (4, 0),  # added
+            }
+        ]
+    },
+}
+clpatt1 = list(D[4].keys())[0]
+mesh1 = D[4][clpatt1][0]
+clpatt2 = list(D[5].keys())[0]
+mesh2 = D[5][clpatt2][0]
+
+mp1 = MeshPatt(clpatt1, mesh1)
+mp2 = MeshPatt(clpatt2, mesh2)
+
+print(mp1.ascii_plot())
+print("------------------")
+print(mp2.ascii_plot())
+
+for i in range(1, 10):
+    for perm in Perm.of_length(i):
+        numic = num_increasing_cycles(perm, q)
+        Foata_of_perm = Foata(perm)
+        nummp = sum(1 for _ in Foata_of_perm.occurrences_of(mp1)) + sum(
+            1 for _ in Foata_of_perm.occurrences_of(mp2)
+        )
+        if numic != nummp:
+            print(perm, numic)
+            print(Foata_of_perm, nummp)
